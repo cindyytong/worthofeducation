b1f545945d4852dd34bf0c9c985277641910928c
Cindy Tong <tongcindyy@gmail.com>
2019-10-16 16:26:31 -0400

    Broken tooltip

--- src/map.js
+++ src/map.js
@@ -4,7 +4,7 @@ import * as rank from './data/top_ten';
 
  //Width and height of map
  var width = 1300;
- var height = 800;
+ var height = 700;
  
  // create svg
  var svg = d3.select( "#map" )
@@ -19,9 +19,9 @@ import * as rank from './data/top_ten';
  
  // D3 Projection
  var albersProjection= d3.geoAlbers()
-     .scale( 1300 )
+     .scale( 1340 )
      .rotate( [90,0] )
-    //  .center( [0, 42.313] )
+     .center( [-6, 38] )
      .translate( [width/2,height/2] );
  
          
@@ -40,32 +40,33 @@ import * as rank from './data/top_ten';
        .attr( "d", geoPath );
 
  
- 
-// Map schools
-// make tooltip for school
-
-// let getTooltip = document.getElementById("tooltip");
+ // Map schools
+ function openModal(){
+    modal.style.display = "block";
+}
 
 let tip = d3.tip()
     .html(function(school) {
         return("<div class='tooltip-content'><h4>"+school.school+"</h4>"+
         "<p>Ranking: "+school.rank+"</p>"+
-        "<a href='#'>See Statistics<a></div>");
+        "<button id='open-modal' onclick='openModal'>See Statistics</button></div>");
     })
+
+// let tip = d3.tip()
+//     .html(function(school) {
+//         return("<div class='tooltip-content'><h4>"+school.school+"</h4>"+
+//         "<p>Ranking: "+school.rank+"</p>")
+
+//     })
+
+// d3.selectAll('.tooltip-content').appendHTML = "<button id='open-modal' onclick='openModal'>See Statistics</button></div>";
 
 svg.call(tip);
 
-//Select All
+//Default have all schools shown
 let schoolPins = svg.append("g");
-
-// Filtering 
-// All 
-function filterSchools(filterId, dataSet) {
-    document.getElementById(filterId).onclick = function(){
-        d3.selectAll("circle").remove();
-
-        schoolPins.selectAll( "path" )
-        .data(dataSet)
+schoolPins.selectAll( "path" )
+        .data(schools.features)
         .enter()
         .append("circle")
         .attr('class', 'default-pin')
@@ -78,32 +79,59 @@ function filterSchools(filterId, dataSet) {
         })
         .on('mouseover', tip.show)
         .on('click', tip.hide);		
+
+// Filtering 
+// Default select all
+function filterSchools(filterId, dataSet, pinClass) {
+    document.getElementById(filterId).onclick = function(){
+        d3.selectAll("circle").remove();
+
+        schoolPins.selectAll( "path" )
+        .data(dataSet)
+        .enter()
+        .append("circle")
+        .attr('class', pinClass)
+        .attr("r", 3)
+        .attr("transform", function(school) {
+            return "translate(" + albersProjection([
+            school.lon,
+            school.lat
+            ]) + ")";
+        })
+        .on('mouseover', tip.show)
+        .on('click', tip.hide);		
     }
 }
 
-filterSchools('all', schools.features);
-filterSchools('tuition', rank.most_expensive);
-filterSchools('start-salary', rank.highest_start);
-filterSchools('mid-salary', rank.highest_midcareer);
-filterSchools('pay-off', rank.fastest_pay);
+filterSchools('all', schools.features, 'default-pin');
+filterSchools('tuition', rank.most_expensive, 'blue-pin');
+filterSchools('start-salary', rank.highest_start, 'red-pin');
+filterSchools('mid-salary', rank.highest_midcareer, 'purple-pin');
+filterSchools('pay-off', rank.fastest_pay, 'pink-pin');
+
+// modal functionality 
+
+let modal = document.getElementById('modal');
+modal.style.display = "none"; // default do not display 
+
+let close = document.getElementsByClassName("close")[0];
 
 
-
-// highest tuition 
-// document.getElementById('tuition').onclick = function(){
-//     schoolPins.selectAll( "path" )
-//     .data(rank.most_expensive)
-//     .enter()
-//     .append("circle")
-//     .attr('class', 'default-pin')
-//     .attr("r", 3)
-//     .attr("transform", function(school) {
-//         return "translate(" + albersProjection([
-//         school.lon,
-//         school.lat
-//         ]) + ")";
-//     })
-//     .on('mouseover', tip.show)
-// 	.on('click', tip.hide);		
+// let btn = document.getElementById('open-modal');
+// btn.onclick = function(){
+//     modal.style.display = "block";
 // }
 
+
+
+
+close.onclick = function(){
+    modal.style.display = "none";
+}
+
+// remove modal if clicking outside of modal
+window.onclick = function(event) {
+    if(event.target != modal) {
+        modal.style.display = "none";
+    }
+}
